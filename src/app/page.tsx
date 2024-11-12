54cb46be1c014797df5cf238be13f6905ec862d4
Muhammad Khurram <khurramsmartboy83@gmail.com>
2024-11-12 14:11:51 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,22 @@ import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
-import AOS from "aos"
+import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect (() => {
+  useEffect(() => {
     AOS.init({
       easing: "ease-out-back",
-      duration:1200,
-      delay:100,
+      duration: 1200,
+      delay: 100,
       mirror: true,
       anchorPlacement: "bottom-bottom",
       offset: 160,
     });
     AOS.refresh();
-
-  },[]);
+  }, []);
 
   return (
     <main>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,40 +1,41 @@
-import { title } from 'process'
-import React from 'react'
-import Heading from './Heading'
-import Card from './Card'
+import React from 'react';
+import Heading from './Heading';
+import Card from './Card';
 
 const data = [
     {
-        id:0,
+        id: 0,
         title: "Todo List",
         desc: "Manage your Work",
         img: "/PP.PNG",
-        tags: ["React", "Node", "CSS","Typescript"],
+        tags: ["React", "Node", "CSS", "Typescript"],
     },
     {
-        id:1,
+        id: 1,
         title: "Resume",
-        desc: "Cariculum Vite",
+        desc: "Curriculum Vitae",
         img: "/PP.PNG",
-        tags: ["React", "Node", "CSS","Typescript"],
+        tags: ["React", "Node", "CSS", "Typescript"],
     },
-]
+];
 
 const Projects = () => {
   return (
     <div id='projects' className='container pt-32'>
-     <Heading Title='My Projects'/>
-     <div className='grid gap-10 xl:gap-0 xl:gap-y-10 md:grid-cols-2 lg:grid-cols-3 place-items-center'>
-        {data.map((el) => (<Card
-        key={el.id}
-        title={el.title}
-        desc={el.desc}
-        img={el.img}
-        tags={el.tags}
-        />))}
-        </div> 
+      <Heading Title='My Projects' />
+      <div className='grid gap-10 xl:gap-0 xl:gap-y-10 md:grid-cols-2 lg:grid-cols-3 place-items-center'>
+        {data.map((el) => (
+          <Card
+            key={el.id}
+            title={el.title}
+            desc={el.desc}
+            img={el.img}
+            tags={el.tags}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
